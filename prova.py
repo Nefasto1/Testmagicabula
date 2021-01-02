f174d1d263919520fe75e7af65b8189fd7fea1ad
repl.it user <replituser@example.com>
2021-01-02 15:56:49 +0000

    Primo commit

---
+++ prova.py
@@ -0,0 +1,16 @@
+c = 2
+d = 5
+
+a = int(input('Inserisci il valore: '))
+
+if a > 10:
+    b = a - (c + d)
+elif a < 5:
+    b = a + (c + d)
+else:
+    b = a + c - d 
+
+print('a: {}' .format(a))
+print('b: {}' .format(b))
+print('c: {}' .format(c))
+print('d: {}' .format(d))
